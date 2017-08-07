389aadd152284ff8707e32e98a487f2005401681
Einars Vilnis <vilniseinars@gmail.com>
2017-08-07 23:49:06 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
+import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  document.querySelector(".reactContainer")
-);
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.reactContainer'));
